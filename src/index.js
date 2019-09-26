4c995a436f8f964d2c4d438e2c48b289b2697b77
Ashley Narcisse <ashley49@gmail.com>
2019-09-26 06:57:08 -0400

    fix(async): fixed json body that was returning as Promise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const {send, json} = require('micro');
 
-const routeHandler = fn => (req, res) => {
+const routeHandler = fn => async (req, res) => {
   const ok = data => send(res, 200, data);
   const badRequest = msg => send(res, 400, msg);
   const unauthorized = msg => send(res, 401, msg);
   const notFound = msg => send(res, 404, msg);
   const error =  err => send(res, 500, err);
-  const body = req.method === 'POST' && json(req) || {};
+  const body = req.method === 'POST' && await json(req) || {};
 
   try {
     return fn({req, res, body, ok, badRequest, unauthorized, notFound, error})
