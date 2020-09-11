11789836ce6152bc6f10c2c2cfdc126372008137
Ashley Narcisse <ashlay49@gmail.com>
2020-09-10 23:12:47 -0400

    fix(micron): fixed issue with default export

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import micron from './micron';
 export * from './micron';
 export * from './createLambda';
 export { default as mockLambda } from './mock';
-export default micron;
+
+module.exports = micron;
